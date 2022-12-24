7c8b53806fdac3b1d838fb73322381e62d686fab
labCap <banlazthosow@gmail.com>
2022-12-24 20:38:31 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-import { FaSun, FaMoon } from "react-icons/fa";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 
 function App() {
   return (
     <>
-      <header>
-        <div className="container">
-          <div className="inner">
-            <div className="logo">
-              <div className="icon-logo" />
-            </div>
-            <button className="theme-btn">
-              <FaSun />
-              <FaMoon />
-            </button>
-          </div>
-        </div>
-      </header>
+      <Header />
 
-      <main>
-        <div className="container">
-          <div className="inner">
-            <h1>This is a site with the ability to change the subject</h1>
-            <h2 className="light">Dark</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptate enim, fuga qui eos iure omnis unde. Et quod iure velit
-              quasi, libero sapiente esse nulla? Qui eaque odit laborum
-              doloremque.
-            </p>
-          </div>
-        </div>
-      </main>
-      <footer>
-        <div class="container">
-          <div className="inner">
-            <div class="powered">
-              Powered by
-              <a href="https://github.com/labCap" target="_blank">
-                labCap
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Main />
+
+      <Footer />
     </>
   );
 }
