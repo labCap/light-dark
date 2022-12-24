a8be49f92f8a0bbdf45c41d6f5a583292349fc5b
labCap <banlazthosow@gmail.com>
2022-12-24 20:37:36 +0200

    added component Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Main.scss";
+
+export const Main = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="inner">
+          <h1>This is a site with the ability to change the subject</h1>
+          <h2 className="light">Dark</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            enim, fuga qui eos iure omnis unde. Et quod iure velit quasi, libero
+            sapiente esse nulla? Qui eaque odit laborum doloremque.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
