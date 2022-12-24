5e90fca56066c1a51c103f99dc1b804425277518
labCap <banlazthosow@gmail.com>
2022-12-24 19:10:23 +0200

    added basic HTML-structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import { FaSun, FaMoon } from "react-icons/fa";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <>
+      <header>
+        <div className="container">
+          <div className="inner">
+            <div className="logo">
+              <div className="icon-logo" />
+            </div>
+            <button className="theme-btn">
+              <FaSun />
+              <FaMoon />
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="container">
+          <div className="inner">
+            <h1>This is a site with the ability to change the subject</h1>
+            <h2 className="light">Dark</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate enim, fuga qui eos iure omnis unde. Et quod iure velit
+              quasi, libero sapiente esse nulla? Qui eaque odit laborum
+              doloremque.
+            </p>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div class="container">
+          <div class="powered">
+            Powered by
+            <a href="https://github.com/labCap" target="_blank">
+              labCap
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
